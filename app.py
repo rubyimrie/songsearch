28e7aa9f85d91fa0e1a6c24c2f84d41bd327ec68
Orlagh Keane <S2084384@ed.ac.uk>
2024-02-13 18:04:15 +0000

    add search result page

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-from flask import url_for
-
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -18,4 +15,17 @@ def index():
     return render_template('search.html', search_query=search_query)
 
 
+@app.route('/search_results', methods=['POST'])
+def search_results():
+    search_query = request.form.get('search_query')
+    # For simplicity, let's assume you have a list of search results
+    # Replace this with your actual search logic
+    search_results = [
+        {"image": "https://i.scdn.co/image/ab67616d0000b2738863bc11d2aa12b54f5aeb36", "song_name": "Blinding Lights", "artist": "The Weeknd", "lyric_sentence": "Lyric sentence 1"},
+        {"image": "https://i1.sndcdn.com/artworks-5VCxiNQdKysNTV2y-Qu5QwQ-t500x500.jpg", "song_name": "Someone you loved", "artist": "Lewis Capaldi", "lyric_sentence": "Lyric sentence 2"},
+        {"image": "https://upload.wikimedia.org/wikipedia/en/b/b0/SoftCellTaintedLove7InchSingleCover.jpg", "song_name": "Tainted Love", "artist": "Soft Cell", "lyric_sentence": "Lyric sentence 3"}
+    ]
+    return render_template('search_results.html', search_query=search_query, search_results=search_results)
 
+if __name__ == "__main__":
+    app.run(debug=True)
